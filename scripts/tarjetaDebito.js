3ce929ea4ac3d7bc5cd3b9e27f66be3700fc367e
Karen Soler <kjsoler19@misena.edu.co>
2022-09-06 11:52:25 -0500

    Cambios 2 06

---
+++ scripts/tarjetaDebito.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const btGuardar= document.getElementById("guardar");
+console.log(btGuardar);
+
+
+btGuardar.addEventListener("click",function(e) {
+    e.preventDefault();
+    let form=document.getElementById("tarjetadebito");
+    let form2=document.getElementById("tarjetadebito");
+    let form3=document.getElementById("tarjetadebito");
+    console.log(form[0].value);
+    console.log(form2[1].value);
+    console.log(form3[2].value);
+});
